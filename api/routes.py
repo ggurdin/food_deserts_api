dad479c89207e6a815654bfac513c8f6355005e9
ggurdin <ggurdin@gmail.com>
2022-10-06 17:32:17 -0400

    moved api calls to client side, buttons to change map data, api key

--- api/routes.py
+++ api/routes.py
@@ -6,11 +6,8 @@ from dotenv import load_dotenv
 from flask import render_template
 import json
 from google.cloud import storage
-import requests
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-import plotly
+from functools import wraps
+from flask.helpers import make_response
 
 
 if os.environ.get('GAE_ENV') == 'standard':
@@ -21,6 +18,8 @@ if os.environ.get('GAE_ENV') == 'standard':
 	db_host = os.environ.get('DB_HOST')
 	unix_socket = '/cloudsql/{}'.format(db_connection_name)
 	cnx = pymysql.connect(user=db_user, password=db_password, unix_socket=unix_socket, db=db_name)
+	api_endpoint = "https://richmondfooddeserts.com/api/"
+	api_key = os.environ.get("API_KEY")
 
 else:
 	load_dotenv()
@@ -29,6 +28,21 @@ else:
 	db_name = os.environ.get('DB_NAME')
 	db_host = os.environ.get('DB_HOST')
 	cnx = pymysql.connect(user=db_user, password=db_password, host=db_host, db=db_name)
+	api_endpoint = "http://127.0.0.1:5000/api/"
+	api_key = os.environ.get("API_KEY")
+
+def token_required(f):
+	@wraps(f)
+	def decorator(*args, **kwargs):
+		token = None
+		if 'x-access-token' in request.headers:
+			token = request.headers['x-access-token']
+		if not token:
+			return make_response({"message": "A valid token is missing"}, 401)
+		elif token != api_key:
+			return make_response({"message": "Invalid token"}, 401)
+		return f(*args, **kwargs)
+	return decorator
 
 def get_geojson():
     client = storage.Client()
@@ -37,50 +51,13 @@ def get_geojson():
     blob = bucket.blob('new_richmond.json')
     return json.loads(blob.download_as_string(client=None))
 
-def get_tract_df():
-	tract_data = requests.get("https://richmondfooddeserts.com/api/tract_demographics/")
-	tract_data = tract_data.json()
-	df = pd.DataFrame.from_records(tract_data["demographics"])
-	df = df[["census_tract", "county", "median_family_income", "population"]]
-	df = df.rename({
-		"census_tract": "Census Tract",
-		"county": "County",
-		"median_family_income": "Median Family Income",
-		"population": "Population"
-	}, axis=1)
-	return df
-
-def create_map(geojson, df):
-	customdata = np.stack((df['Census Tract'], df['County'], df['Median Family Income'].astype(int)), axis=-1)
-	fig = go.Figure(data=go.Choropleth(
-		locations=df['Census Tract'],
-		geojson=geojson,
-		z = df['Population'],
-		featureidkey="properties.GEO_ID",
-		showscale=False,
-		customdata=customdata,
-		hovertemplate = (
-			'Census Tract: %{customdata[0]}<br>' +
-			'County: %{customdata[1]}<br>' + 
-			'Population: %{customdata[2]}<br>' + 
-			'<extra></extra>'
-		)
-	))    
-	fig.update_geos(fitbounds="locations", showcoastlines=False, showland=False, showframe=False)
-	fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-	fig.update_layout(dragmode=False)
-	return fig
-
-
 @app.route("/")
 def index():
 	geojson = get_geojson()
-	df = get_tract_df()
-	map = create_map(geojson, df)
-	map = json.dumps(map, cls=plotly.utils.PlotlyJSONEncoder)
-	return render_template("map.html", map=map)
+	return render_template("map.html", geojson=geojson, api_endpoint=api_endpoint, api_key=api_key)
 
 @app.route("/api/<type>/")
+@token_required
 def urban(type):
 	if type not in ["urban", "rural"]:
 		return {"status": "not found"}
@@ -100,6 +77,7 @@ def urban(type):
 	return tract_ids
 
 @app.route("/api/low_income/")
+@token_required
 def low_income():
 	county = None
 	if "county" in request.args:
@@ -108,13 +86,13 @@ def low_income():
 		sql = "SELECT tract_id FROM food_desert_data WHERE low_income = 1"
 		if county:
 			sql = sql + f' AND LOWER(county) LIKE "{county}";'
-		print(sql)
 		cursor.execute(sql)
 		ret = cursor.fetchall()
 		tract_ids = {"tracts": [x[0] for x in ret]}
 	return tract_ids
 
 @app.route("/api/<lila>/<distance>/")
+@token_required
 def li_distance(lila, distance):
 	if lila not in ["low_income", "low_access"]:
 		return {"status": "not found"}
@@ -135,6 +113,7 @@ def li_distance(lila, distance):
 	return tract_ids
 
 @app.route("/api/<lila>/vehicle")
+@token_required
 def li_vehicle(lila):
 	if lila not in ["low_income", "low_access"]:
 		return {"status": "not found"}
@@ -153,6 +132,7 @@ def li_vehicle(lila):
 	return tract_ids
 
 @app.route("/api/<lila>/<distance>/<type>")
+@token_required
 def li_distance_type(lila, distance, type):
 	if lila not in ["low_income", "low_access"]:
 		return {"status": "not found"}
@@ -176,6 +156,7 @@ def li_distance_type(lila, distance, type):
 	return tract_ids
 
 @app.route("/api/tract_demographics/")
+@token_required
 def tract_info():
 	sql = f"""
 		SELECT 
@@ -209,41 +190,46 @@ def tract_info():
 		columns = cursor.description 
 		resp = list(cursor.fetchall())
 		columns = [x[0] for x in columns]
-		print(resp)
-		print(columns)
 		ret = {"demographics": []}
 		for tract in resp:
 			ret["demographics"].append({key: value for key, value in zip(columns, tract)})
 	return ret
 
 @app.route("/api/access/<distance>")
+@token_required
 def access(distance):
-	if "tract_id" not in request.args:
-		return {"status": "not found"}
-	tract_id = int(request.args["tract_id"])
+	sql = f"""
+		SELECT
+			tract_id,
+			county,
+			la_pop_{distance} AS low_access_population,
+			lali_pop_{distance} AS low_access_low_income_population,
+			la_kids_{distance} AS low_access_child_population,
+			la_seniors_{distance} AS low_access_senior_population,
+			la_white_{distance} AS low_access_white_population,
+			la_black_{distance} AS low_access_black_population,
+			la_asian_{distance} AS low_access_asian_population,
+			la_pacific_{distance} AS low_access_hawaiian_and_pacific_islander_population,
+			la_native_{distance} AS low_access_native_american_and_native_alaskan_population,
+			la_other_{distance} AS low_access_mixed_race_and_other_population,
+			la_latino_{distance} AS low_access_hispanic_and_latino_population,
+			la_vehicle_{distance} AS housing_units_without_vehicle_access,
+			la_snap_{distance} AS low_access_housing_units_on_snap
+		FROM food_desert_data
+	"""
+	if "tract_id" in request.args:
+		tract_id = int(request.args["tract_id"])
+		sql += f" WHERE tract_id = {tract_id}"
+	
 	if distance not in ["half_mile", "one_mile", "ten_miles", "twenty_miles"]:
 		return {"status": "not found"}
+
 	with cnx.cursor() as cursor:
-		sql = f"""
-			SELECT
-				la_pop_{distance} AS low_access_population,
-				lali_pop_{distance} AS low_access_low_income_population,
-				la_kids_{distance} AS low_access_child_population,
-				la_seniors_{distance} AS low_access_senior_population,
-				la_white_{distance} AS low_access_white_population,
-				la_black_{distance} AS low_access_black_population,
-				la_asian_{distance} AS low_access_asian_population,
-				la_pacific_{distance} AS low_access_hawaiian_and_pacific_islander_population,
-				la_native_{distance} AS low_access_native_american_and_native_alaskan_population,
-				la_other_{distance} AS low_access_mixed_race_and_other_population,
-				la_latino_{distance} AS low_access_hispanic_and_latino_population,
-				la_vehicle_{distance} AS housing_units_without_vehicle_access,
-				la_snap_{distance} AS low_access_housing_units_on_snap
-			FROM food_desert_data
-			WHERE tract_id = {tract_id}
-		"""
 		cursor.execute(sql)
 		columns = cursor.description 
-		resp = list(cursor.fetchone())
+		resp = list(cursor.fetchall())
 		columns = [x[0] for x in columns]
-		return {key: value for key, value in zip(columns, resp)}
+		ret = {"demographics": []}
+		for tract in resp:
+			ret["demographics"].append({key: value for key, value in zip(columns, tract)})
+	return ret
